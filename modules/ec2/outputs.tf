13e6fdafbd90a8cf7c2b0f48d1957c0908fa3278
Slawomir Nowakowski <snowak@softserveinc.com>
2023-07-19 08:42:52 +0200

    Fianl commit

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,6 +1,5 @@
 output "ec2_instance_ids" {
   description = "IDs of the created EC2 instances"
-#  value       = aws_instance.ec2_instance[*].id
   value = tomap({
     for k, id in aws_instance.ec2_instance : k => id
   })
--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -1,6 +1,5 @@
 output "subnet_ids" {
   description = "IDs of created subnets"
-  #value       = values(aws_subnet.subnet[*]).id
   value = tomap({
     for k, id in aws_subnet.subnet : k => id
   })
